075a4408702b806842a889ea2c7bb3f62526f0bd
Rickard Laurin <rickard.laurin@gmail.com>
2018-04-20 07:41:04 +0200

    feat: add hover effect to button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 
 const Button = styled(Link)`
   background-color: ${({ theme }) => theme.colors.radicalRed};
+  border: 3px solid transparent;
   border-radius: 6px;
   color: #fff;
   display: inline-block;
@@ -13,6 +14,14 @@ const Button = styled(Link)`
   padding: 15px 20px;
   text-align: center;
   text-decoration: none;
+  transition-property: background-color, border-color;
+  transition-timing-function: ease-in-out;
+  transition-duration: 150ms;
+
+  &:hover {
+    background-color: transparent;
+    border-color: #fff;
+  }
 `
 
 export default Button
