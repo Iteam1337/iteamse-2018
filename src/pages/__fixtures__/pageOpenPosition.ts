1896a0e8fa50fd381cecfdb857b5a6ac15fa5107
André Elmoznino Laufer <andre.elmoznino@iteam.se>
2018-06-01 10:39:26 +0200

    feat: list perks on openposition pages (#4)

--- src/pages/__fixtures__/pageOpenPosition.ts
+++ src/pages/__fixtures__/pageOpenPosition.ts
@@ -9,6 +9,7 @@ export const pageOpenPosition = {
   bonusKnowledge:
     '- Kvantitativa researchmetoder (t.ex. enkäter och användningsdata i Google Analytics)\n- Förståelse för den tekniska miljö dina lösningar kommer att implementeras i (t.ex. grundläggande HTML och CSS)\n- Utbildning inom interaktionsdesign, människa-datorinteraktion, beteendevetenskap, kognitionspsykologi, digital design eller närliggande område',
   bonusKnowledgeTitle: 'Plus i kanten',
+  contactTitle: 'Frågor?',
   contentImage:
     '//images.ctfassets.net/rj4r6yfcesw5/62hgzE8G3ueMa2YSEuG4og/db2b643db6041f9d26936bc2d6006607/bleed_howwework.jpg',
   headerImage:
@@ -20,6 +21,8 @@ export const pageOpenPosition = {
     '- Kvalitativa researchmetoder (t.ex. användbarhetstester, observationer, intervjuer och workshops)\n- Kommunicera dina insikter med hjälp av lämpliga leverabler (t.ex. rapporter, användarresor, prototyper eller skrynkliga pappersskisser)\n- Interaktionsdesign\n- Vara användarens representant i projekt och stötta både kollegor och kunder med användarens perspektiv\n- Jobba agilt\n- Arbeta korsfunktionellt och förstå behoven hos kollegor ur andra yrkesgrupper\n- Goda kunskaper i engelska (för att kunna kommincera med dina kollegor)\n- Goda kunskaper i svenska (för att kunna genomföra användarstudier i kontexter där det primära användarspråket är svenska)',
   knowledgeTitle: 'Du behöver kunna',
   location: 'Stockholm',
+  perks: `- 6 veckors semester\n- Kodluncher varje fredag för gemensamt lärande inkl lunch\n- Ingen förväntad övertid\n- Förmånligt friskvårdsbidrag och fokus på välmående\n- Förmånligt pensionssystem\n- Bonus vid slutet av ett bra år\n - Mentorskap mellan arbetskamrater\n- Individuell budget för kompetensutveckling`,
+  perksTitle: 'Några av våra bonusar',
   role:
     'Som UX designer på Iteam är din roll att hjälpa våra kunder med digitala lösningar som skapar värde på riktigt. Som gör något bra både för kunden, slutanvändaren och samhället. Med hjälp av olika researchmetoder undersöker du både användarens och kundens behov och kontext, samt definierar och testar användarupplevelser.\n\nDu delar med dig av din kunskap inom user experience. Du förstår också att om lösningar ska vara verkligt användarvänliga behöver även arbetsprocesserna och kulturen vara användarcentrerad. Vi strävar mot att UX aldrig ska få vara bara en fas eller en eftertanke. Det ska genomsyra hela projektet. Alltid. ',
   roleTitle: 'Din roll',
