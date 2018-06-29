09f7130486a77043b45fc67af0b876d6459540a3
André Elmoznino Laufer <andre.elmoznino@iteam.se>
2018-06-29 16:27:39 +0200

    feat: started drafting redirects (#9)

--- src/server.tsx
+++ src/server.tsx
@@ -13,6 +13,7 @@ import { ServerStyleSheet, ThemeProvider } from 'styled-components'
 import App from './App'
 import Html from './Html'
 import { theme } from './theme'
+import { redirectHelper } from './utils/serverHelpers'
 
 const server = express()
 
@@ -21,54 +22,58 @@ server.use(compression())
 server
   .disable('x-powered-by')
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR!))
-  .get('/*', async (req: express.Request, res: express.Response) => {
-    try {
-      const client = new ApolloClient({
-        cache: new InMemoryCache(),
-        link: createHttpLink({
-          fetch,
-          uri: process.env.RAZZLE_CMS_NODE_URL,
-        }),
-        ssrMode: true,
-      })
+  .get(
+    '/*',
+    redirectHelper,
+    async (req: express.Request, res: express.Response) => {
+      try {
+        const client = new ApolloClient({
+          cache: new InMemoryCache(),
+          link: createHttpLink({
+            fetch,
+            uri: process.env.RAZZLE_CMS_NODE_URL,
+          }),
+          ssrMode: true,
+        })
 
-      const sheet = new ServerStyleSheet()
-      const context = {}
+        const sheet = new ServerStyleSheet()
+        const context = {}
 
-      const markup = sheet.collectStyles(
-        <ApolloProvider client={client}>
-          <ThemeProvider theme={theme}>
-            <StaticRouter context={context} location={req.url}>
-              <App />
-            </StaticRouter>
-          </ThemeProvider>
-        </ApolloProvider>
-      )
+        const markup = sheet.collectStyles(
+          <ApolloProvider client={client}>
+            <ThemeProvider theme={theme}>
+              <StaticRouter context={context} location={req.url}>
+                <App />
+              </StaticRouter>
+            </ThemeProvider>
+          </ApolloProvider>
+        )
 
-      const content = await renderToStringWithData(markup)
+        const content = await renderToStringWithData(markup)
 
-      const apolloState = client.extract()
-      const styleTags = sheet.getStyleElement()
+        const apolloState = client.extract()
+        const styleTags = sheet.getStyleElement()
 
-      const html = (
-        <Html
-          apolloState={serialize(apolloState, { isJSON: true })}
-          content={content}
-          styleTags={styleTags}
-        />
-      )
+        const html = (
+          <Html
+            apolloState={serialize(apolloState, { isJSON: true })}
+            content={content}
+            styleTags={styleTags}
+          />
+        )
 
-      res.send(`<!doctype html>\n${renderToStaticMarkup(html)}`)
-    } catch (error) {
-      console.error(error)
+        res.send(`<!doctype html>\n${renderToStaticMarkup(html)}`)
+      } catch (error) {
+        console.error(error)
 
-      res.status(500)
-      res.end(
-        `An error occurred. Please submit an issue to [https://github.com/apollographql/GitHunt-React] with the following stack trace:\n\n${
-          error.stack
-        }`
-      )
+        res.status(500)
+        res.end(
+          `An error occurred. Please submit an issue to [https://github.com/apollographql/GitHunt-React] with the following stack trace:\n\n${
+            error.stack
+          }`
+        )
+      }
     }
-  })
+  )
 
 export default server
