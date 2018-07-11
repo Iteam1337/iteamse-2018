927a6f10c863f392c90d6fd4b5eedb75cc6e3bdc
André Elmoznino Laufer <andre.elmoznino@iteam.se>
2018-07-11 13:33:42 +0200

    feat: add open positions block on How We Work page (#16)

---
+++ src/components/Blocks/OpenPositionsBlock.tsx
@@ -0,0 +1,95 @@
+import * as React from 'react'
+import Fade from 'react-reveal/Fade'
+import { HowWeWorkPageQuery } from '../../../typings/iteamse'
+import { OPEN_POSITION_PAGE_QUERY } from '../../pages/OpenPosition'
+import styled from '../../theme'
+import GridColumn from '../Grid/GridColumn'
+import PaddedRow from '../Grid/PaddedRow'
+import StyledPrefetchLink from '../Link/StyledPrefetchLink'
+import UnstyledList from '../List/UnstyledList'
+import H3 from '../Typography/H3'
+import H4 from '../Typography/H4'
+import Paragraph from '../Typography/Paragraph'
+
+interface OpenPositionsBlockProps {
+  'data-test'?: string
+  openApplicationHeader: string
+  openApplicationText: string
+  openpositions: HowWeWorkPageQuery['openpositions']
+  title: string
+}
+
+const ColoredGridColumn = GridColumn.extend`
+  background-color: ${({ theme }) => theme.colors.concrete};
+`
+
+const Content = styled.div`
+  display: grid;
+  grid-row-gap: 20px;
+
+  @media (min-width: 1025px) {
+    grid-column-gap: 60px;
+    grid-template-columns: repeat(2, 1fr);
+    grid-row-gap: 0;
+  }
+`
+
+const SideWrap = styled.div``
+
+const UnstyledListWithMargin = UnstyledList.extend`
+  margin-top: 10px;
+`
+
+const ParagraphWrap = styled.div`
+  margin-top: 20px;
+`
+
+const OpenPosition = styled.li`
+  line-height: 44px;
+`
+
+const OpenPositionsBlock: React.SFC<OpenPositionsBlockProps> = ({
+  'data-test': dataTest = '',
+  openApplicationHeader,
+  openApplicationText,
+  openpositions,
+  title,
+}) => {
+  return (
+    <Fade bottom distance="50px">
+      <ColoredGridColumn>
+        <PaddedRow data-test={`block-${dataTest}`}>
+          <Content>
+            <SideWrap>
+              <H3>{title}</H3>
+              <UnstyledListWithMargin>
+                {openpositions.map(position => (
+                  <OpenPosition key={position.id}>
+                    <StyledPrefetchLink
+                      query={OPEN_POSITION_PAGE_QUERY}
+                      to={`/jobba-hos-oss/${position.id}`}
+                      variables={{
+                        id: position.id,
+                      }}
+                    >
+                      {position.title}
+                    </StyledPrefetchLink>{' '}
+                    ({position.location})
+                  </OpenPosition>
+                ))}
+              </UnstyledListWithMargin>
+            </SideWrap>
+            <SideWrap>
+              <H4>{openApplicationHeader}</H4>
+              <ParagraphWrap>
+                <Paragraph>{openApplicationText}</Paragraph>
+              </ParagraphWrap>
+            </SideWrap>
+          </Content>
+        </PaddedRow>
+      </ColoredGridColumn>
+    </Fade>
+  )
+}
+
+export default OpenPositionsBlock
--- src/pages/HowWeWork.tsx
+++ src/pages/HowWeWork.tsx
@@ -6,6 +6,7 @@ import { HowWeWorkPageQuery } from '../../typings/iteamse'
 import Block from '../components/Blocks/Block'
 import ImageBleed from '../components/Blocks/ImageBleed'
 import ImageBlock from '../components/Blocks/ImageBlock'
+import OpenPositionsBlock from '../components/Blocks/OpenPositionsBlock'
 import GridColumn from '../components/Grid/GridColumn'
 import Header from '../components/Header/Header'
 import Team from '../components/Team/Team'
@@ -15,6 +16,11 @@ import howWeWorkImage from './img/illustrations-group-3.svg'
 
 export const HOW_WE_WORK_PAGE_QUERY = gql`
   query HowWeWorkPage {
+    openpositions {
+      location
+      id
+      title
+    }
     pageHowWeWork {
       headerImage
       headerText1
@@ -24,6 +30,8 @@ export const HOW_WE_WORK_PAGE_QUERY = gql`
       customersText
       customersTitle
       hiringTitle
+      openApplicationText
+      openApplicationHeader
       imageBleed
       methodText
       methodTitle
@@ -51,20 +59,14 @@ export class HowWeWork extends React.Component {
             return null
           }
 
-          const { pageHowWeWork } = data
+          const { openpositions, pageHowWeWork } = data
 
           return (
             <>
               <Helmet>
                 <title>Iteam | Hur vi jobbar</title>
-                <meta
-                  name="og:title"
-                  content="Iteam | Hur vi jobbar"
-                />
-                <meta
-                  name="twitter:title"
-                  content="Iteam | Hur vi jobbar"
-                />
+                <meta name="og:title" content="Iteam | Hur vi jobbar" />
+                <meta name="twitter:title" content="Iteam | Hur vi jobbar" />
                 {pageHowWeWork.headerImage && (
                   <meta
                     name="og:image"
@@ -87,7 +89,15 @@ export class HowWeWork extends React.Component {
                 <Block title={pageHowWeWork.methodTitle}>
                   {pageHowWeWork.methodText}
                 </Block>
-                <ImageBleed image={pageHowWeWork.imageBleed} />
+              </GridColumn>
+              <OpenPositionsBlock
+                openApplicationText={pageHowWeWork.openApplicationText}
+                openApplicationHeader={pageHowWeWork.openApplicationHeader}
+                openpositions={openpositions}
+                title={pageHowWeWork.hiringTitle}
+              />
+              <ImageBleed image={pageHowWeWork.imageBleed} />
+              <GridColumn>
                 <Block title={pageHowWeWork.sharingTitle}>
                   {pageHowWeWork.sharingText}
                 </Block>
